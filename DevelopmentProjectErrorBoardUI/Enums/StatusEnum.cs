f55787c303875a1e829c9bbeb3d9002fe4c89e1a
Ahmed-20-02 <asaeed2002.as@gmail.com>
2024-04-07 19:56:11 +0100

    Added default value to status dropdown
    Reading in current status value and setting it to avoid incorrect display of data

---
+++ DevelopmentProjectErrorBoardUI/Enums/StatusEnum.cs
@@ -0,0 +1,13 @@
+namespace DevelopmentProjectErrorBoardUI.Enums
+{
+    public enum StatusEnum
+    {
+        InProgress = 1,
+        AwaitingInformation = 2,
+        Escalated = 3,
+        Cancelled = 4,
+        Resolved = 5
+        
+        
+    }
+}
